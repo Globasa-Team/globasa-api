2873dac7a42c08ca81bf17899858852fd4a79239
shawn@snowbank.ca <shawn@snowbank.ca>
2023-10-24 13:44:18 +0000

    :memo: update documentation

--- cli-update.php
+++ cli-update.php
@@ -4,17 +4,21 @@ namespace globasa_api;
 use Throwable;
 
 try {
+    // Startup
     $app_path = __DIR__;
     require_once("{$app_path}/init.php");
     $c['log']->add("Using: " . $argv[1]);
     $c['log']->add("Environment: " . ($c['dev'] ? 'dev' : 'production'));
+    
+    // Update data files
     $csv_data = Update_controller::load_current_terms($c, $argv[1]);
     Update_controller::log_changes($csv_data, $data['previous'], $c);
     Update_controller::update_i18n($c);
+
+    // Finish up
     $c['log']->add("Script complete");
     $c['log']->email_log($c);
     yaml_emit_file(DATA_FILENAME, ['previous'=>$argv[1]]);
-    
 }
 catch (Throwable $e) {
     echo("\nCAUGHT
