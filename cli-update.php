84d9f51a0f29444678f1103595e6f60bf87002c2
shawn@snowbank.ca <shawn@snowbank.ca>
2023-10-24 13:13:17 +0000

    :sparkles: add environment to log

--- cli-update.php
+++ cli-update.php
@@ -5,8 +5,9 @@ use Throwable;
 
 try {
     $app_path = __DIR__;
-    echo("Using: " . $argv[1] . "\n");
     require_once("{$app_path}/init.php");
+    $c['log']->add("Using: " . $argv[1]);
+    $c['log']->add("Environment: " . ($c['dev'] ? 'dev' : 'production'));
     $csv_data = Update_controller::load_current_terms($c, $argv[1]);
     Update_controller::log_changes($csv_data, $data['previous'], $c);
     yaml_emit_file(DATA_FILENAME, ['previous'=>$argv[1]]);
