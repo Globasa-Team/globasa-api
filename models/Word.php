ce08b085853cc3a7a953272fd6c71e80cd811491
PartialShawn <shawn@snowbank.ca>
2025-09-02 18:46:09 +0000

    add get final morpheme function

---
+++ models/Word.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace globasa_api;
+
+class Word
+{
+
+    /**
+     * Returns the final part of derived etymology, or the term, or the slug.
+     */
+    public static function get_final_morpheme(string $entry_slug): string
+    {
+        global $dict;
+        $morpheme = "";
+
+        if (isset($dict[$entry_slug]['etymology']['derived'])) {
+            $morpheme = $dict[$entry_slug]['etymology']['derived'][array_key_last($dict[$entry_slug]['etymology']['derived'])];
+        } elseif (isset($dict[$entry_slug]['term'])) {
+            $morpheme = mb_strtolower($dict[$entry_slug]['term']);
+        } else {
+            $morpheme = $entry_slug;
+            trigger_error("Tried to get final morpheme, but entry doesn't exist in dictionary word list.");
+            \pard\m("Tried to get final morpheme, but entry doesn't exist in dictionary word list.", 'Error', true);
+        }
+        return $morpheme;
+    }
+}
