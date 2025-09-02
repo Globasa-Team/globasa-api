d95143fef911fc3daf6d5cca4ab4b1ab60b915f6
PartialShawn <shawn@snowbank.ca>
2025-09-02 18:48:20 +0000

    leave underscores, space to underscore

--- helpers/slugify.php
+++ helpers/slugify.php
@@ -10,26 +10,44 @@
  * Spaces work sometimes but it's probably best to hypenize them
  * 
  * https://stackoverflow.com/questions/2955251/php-function-to-make-slug-url-string/13331948#13331948
+ * https://unicode-org.github.io/icu/userguide/transforms/general/
+ * See function below's reference, also
  */
-function slugify(string $text) {
-
-    $rules = <<<'RULES'
-        :: Any-Latin;
+function slugify(string $text)
+{
+    /**
+     * Heredoc string literal of the rules
+     *
+     * :: NFD;
+     * :: [:Nonspacing Mark:] Remove;
+     * :: NFC;
+     *      `NFD`/`NFC` markers for Normalization Form something-or-other.
+     *      `[:Nonspacing Mark:] Remove;` remove diacritics/accents.
+     * :: Lower();
+     *      makes string lower case
+     * :: Any-Latin;
+     *      where possible, converts unicode to asci representation,
+     *      eg « → ‘«’, © → ‘(C)’, Æ → AE
+     * :: [^-\_[:^Punctuation:]] Remove;
+     *      Remove all punctuation except hyphens and underscores.
+     *      Literally remove everything that is not a hyphen, underscore or not punctuation.
+     *      Note double negative (not not puncutation).
+     * [:White_Space:] > '_';
+     *      Replace all white space with underscores
+     */
+    $rules = <<<RULES
         :: NFD;
         :: [:Nonspacing Mark:] Remove;
         :: NFC;
-        :: [^-[:^Punctuation:]] Remove;
         :: Lower();
-    RULES;
-    
-    return str_replace(' ', '_', 
-                \Transliterator::createFromRules($rules)
-                    ->transliterate(
-                        strtolower(trim($text))
-                        )
-            );
-    
+        :: Any-Latin;
+        :: [^-\_[:^Punctuation:]] Remove;
+        [:White_Space:] > '_';
+        RULES;
+
+    return \Transliterator::createFromRules($rules)->transliterate($text);
 }
+
 
 /**
  * Takes string and converst it to lower case ASCII, trim spaces,
@@ -41,8 +59,8 @@ function slugify(string $text) {
  * 
  * https://stackoverflow.com/questions/2955251/php-function-to-make-slug-url-string/13331948#13331948
  */
-function slugify_strict(string $text) {
-
+function slugify_strict(string $text)
+{
     $rules = <<<'RULES'
         :: Any-Latin;
         :: NFD;
@@ -52,8 +70,7 @@ function slugify_strict(string $text) {
         :: Lower();
         [-[:Separator:]]+ > '-';
     RULES;
-    
+
     return \Transliterator::createFromRules($rules)
-        ->transliterate( $text );
-    
+        ->transliterate($text);
 }
