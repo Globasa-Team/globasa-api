770fe4b90c55d7dc8293e01a0d5b315977a0f8cb
PartialShawn <shawn@snowbank.ca>
2026-03-10 19:32:32 +0000

    bug fix: don't cut off brackets in translation

--- models/Term_parser.php
+++ models/Term_parser.php
@@ -695,13 +695,10 @@ class Term_parser
                         // $segment_length = $len - $start;
                         $segment_length = null;
                     $term = trim(substr($translations, $start, $segment_length));
-                    if ($raw['term']==='exjeksi') var_dump($term);
 
                     // Remove word class and column
                     if ($colon_pos === null) {
                         $term_v2 = $term;
-                        if ($raw['term']==='exjeksi') \pard\m($term_v2, 'No colon');
-
                     } else {
                         $end_shift = 0;
                         if ($translations[$pos] === ',' || $translations[$pos] === ';' ) {
@@ -709,7 +706,6 @@ class Term_parser
                         }
                         $term_v2 = trim(substr($translations, $colon_pos + 1, $pos - $colon_pos + $end_shift));
                         $colon_pos = null;
-                        if ($raw['term']==='exjeksi') \pard\m($term_v2, 'Yes colon');
                     }
 
                     // Add current translation to search term index
